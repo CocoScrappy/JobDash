d148211e98ee6cbec6dbe01c972576a13a0c3cee
Louis <louis.dumasv@gmail.com>
2022-11-25 16:29:11 -0500

    add company to job posting

--- job_posting/models.py
+++ job_posting/models.py
@@ -3,23 +3,26 @@ from datetime import datetime
 from django_config import settings
 
 # Create your models here.
+
+
 class JobPost(models.Model):
     REMOTE_OPTIONS = [
         ('remote', 'Remote'),
         ('hybrid', 'Hybrid'),
         ('in-person', 'In-Person'),
     ]
-     
-    employer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
-    
+
+    employer = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+
     title = models.CharField(max_length=250)
-    logo_url = models.URLField(blank=True)
-    location = models.CharField(max_length= 100)
+    logo_url = models.URLField(null=True)
+    location = models.CharField(max_length=100)
     description = models.TextField(blank=False)
+    company = models.CharField(max_length=60, default='')
     date_created = models.DateTimeField(default=datetime.now())
-    
+
     remote_option = models.CharField(
         max_length=30,
         choices=REMOTE_OPTIONS,
     )
-    
