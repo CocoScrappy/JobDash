f6d3214c9cea224462b2b5f61a2c06b525f91b8c
SpicyRiceTea <chaoukirami@live.ca>
2022-11-30 00:18:41 -0500

    added capacity to search for matches in title OR content of job post

--- job_posting/views.py
+++ job_posting/views.py
@@ -1,9 +1,13 @@
+import sys
 from django.shortcuts import render
 from rest_framework import viewsets,status,mixins
 from . import serializers
 from .models import JobPost
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.core.serializers import serialize
+import json
+from django.db.models import Q
 
 # Create your views here.
 class DefaultJobPostView(viewsets.ModelViewSet):
@@ -20,7 +24,19 @@ class JobSearchView(APIView):
 
 
     def get(self,request,par):
-        searchTerms=par
+        
+        searchTerms=par.split()
+        query=None
+        for t in searchTerms:
+            q=JobPost.objects.filter(Q(title__icontains=t)|Q(description__icontains=t))
+            
+            if query==None:
+                query=q
+            else:
+                query=query|q
+        
+        jsonquery=json.loads(serialize('json',query))
+        
 
-        query=JobPost.objects.get(title__icontains=searchTerms)
-        return Response(query,status=status.HTTP_200_OK)
+        print(searchTerms, file=sys.stderr)
+        return Response(jsonquery,status=status.HTTP_200_OK)
