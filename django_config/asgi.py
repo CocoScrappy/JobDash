be6cc229dc0e04d9f4c0e2dc80369dcfce4af5cd
SpicyRiceTea <chaoukirami@live.ca>
2022-12-06 13:33:56 -0500

    moved a line in asgi

--- django_config/asgi.py
+++ django_config/asgi.py
@@ -8,13 +8,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_config.settings')
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 import job_posting.websocket.routing as routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_config.settings')
+
 
 application = ProtocolTypeRouter(
     {
