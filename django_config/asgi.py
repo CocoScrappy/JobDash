9f8b7c1612b81eec6df89fc7312ad805b57b6adc
SpicyRiceTea <chaoukirami@live.ca>
2022-12-06 14:49:10 -0500

    followed solution as per https://stackoverflow.com/questions/66815416/django-appregistrynotready-apps-arent-loaded-yet-when-deploying-to-heroku inside asgi file

--- django_config/asgi.py
+++ django_config/asgi.py
@@ -7,11 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
-import os
 import django
-
-django.setup()
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_config.settings')
+django.setup()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
