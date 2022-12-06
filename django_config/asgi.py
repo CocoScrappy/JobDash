481cb482706a7b63f4d31c0f2098f33cdcaf8c88
SpicyRiceTea <chaoukirami@live.ca>
2022-12-06 14:52:24 -0500

    minor asgi change

--- django_config/asgi.py
+++ django_config/asgi.py
@@ -7,11 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
-import django
+
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_config.settings')
+import django
 django.setup()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
