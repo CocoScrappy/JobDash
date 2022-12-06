6970bff1e043ff4821c2b9fe49553decc6223d52
SpicyRiceTea <chaoukirami@live.ca>
2022-12-06 14:05:35 -0500

    small change in asgi.py

--- django_config/asgi.py
+++ django_config/asgi.py
@@ -7,15 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 import channels.layers
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_config.settings')
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 import job_posting.websocket.routing as routing
 
-
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_config.settings')
 
 application = ProtocolTypeRouter(
     {
