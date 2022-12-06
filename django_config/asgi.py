26c756424b960ddd8d5147948bc9df0e7c972403
SpicyRiceTea <chaoukirami@live.ca>
2022-12-06 15:12:03 -0500

    forgot to save last commit

--- django_config/asgi.py
+++ django_config/asgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 
 from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_config.settings')
+django_asgi_app = get_asgi_application()
 import django
 django.setup()
 from channels.auth import AuthMiddlewareStack
